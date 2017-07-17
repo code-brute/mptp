52836d5a8b681d82808698c30dbcf6bc2a8cfa39
jackson <m18292930341@163.com>
2017-07-17 22:19:24 +0800

    first commit

---
+++ src/test/java/com/mptp/MptpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mptp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MptpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
